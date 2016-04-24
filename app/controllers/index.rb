82e87255e01f355d23e36fe99ad3ad5b5de88803
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-04-24 08:36:19 -0700

    Create user functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,11 @@
 get '/' do
 	@random = (0...10).map { (65 + rand(26)).chr }.join.downcase
+	session[:identifier] = @random
 	erb :landing
 end
 
 get '/:id' do 
 	id = params[:id]
+	User.create(identifier: id)
 	erb :index
 end
