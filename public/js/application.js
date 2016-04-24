aef72fb7e578f599b4586a34c2cc07d7536b435e
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-04-23 22:43:51 -0700

    Finish add form

--- public/js/application.js
+++ public/js/application.js
@@ -10,39 +10,10 @@ $(document).ready(function() {
 		$('.home-explanation').hide();
 		$('.schema-explanation').show();
 	})
-	
+
+	$('.another-col').click(function(e){
+		e.preventDefault();
+		$(this).parent().parent().append("<p><input type='text' name='header'><select><option value='integer'>Integer</option><option value='text'>Text</option><option value='datetime'>Datetime</option></select><label><input type='checkbox' id='foreignkey' value='foreignkey'>foreign key?</label><label><input type='checkbox' id='presence' value='presence'>presence required?</label><label><input type='checkbox' id='unique' value='unique'>unique required?</label><label><input type='checkbox' id='poly' value='poly'>polymorphic true?</label></p>")
+	})
+
 });
-
-	// $('.modal-trigger').leanModal();
-
-	// $('.add-col').click(function(e){
-	// 	e.preventDefault();
-	// 	$('.create-table-form').append()
-	// })
-
-	// $('.dropdown-button').dropdown({
- //      inDuration: 300,
- //      outDuration: 225,
- //      constrain_width: false, // Does not change width of dropdown to that of the activator
- //      hover: true, // Activate on hover
- //      gutter: 0, // Spacing from edge
- //      belowOrigin: true, // Displays dropdown below the button
- //      alignment: 'left' // Displays dropdown with edge aligned to the left of button
- //    })
-
- //    $('.dropdown-item').click(function(e){
- //    	e.preventDefault();
- //    	var choice = $(this).text();
- //    	var thisclass = $(this).parent().parent()
-
- //    	thisclass.hide();
- //    	thisclass.prev().removeClass('dropdown-button red btn col s4').hide();
-
- //    	var dropdownclass = thisclass.attr('class').split(" ")[0];
-
-	// 	$("<div class='choicefromdrop'>"+choice+"</div>").insertAfter('.'+dropdownclass);
-
-	// 	$(".choicefromdrop").css('margin','0 10px');
-
-
- //    })
