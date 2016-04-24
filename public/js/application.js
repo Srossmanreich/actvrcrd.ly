8b771ff133189bd7e6df0375e065f62651574e30
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-04-24 00:08:57 -0700

    Split homepage route

--- public/js/application.js
+++ public/js/application.js
@@ -3,15 +3,6 @@ $(document).ready(function() {
 	// window.onbeforeunload = function() {
  //  	return "You are attempting to leave this page.";}
 
-	$('.schema-explanation').hide();
-  
-	$('.get-started').click(function(e){
-		e.preventDefault();
-		$('.home-explanation').hide();
-		$('.schema-explanation').show();
-		$('.another-tab').hide();
-	})
-
 	$('.another-col').click(function(e){
 		e.preventDefault();
 		var toAddCols = $('.morecolumns').html();
@@ -26,8 +17,14 @@ $(document).ready(function() {
 
 	$('.submit-tables').click(function(e){
 		e.preventDefault();
+		var link = $('.addtable').attr('action');
 		var input = $('.addtable').serializeArray();
-		console.log(input);
+		
+		var request = $.ajax({
+	      method: "POST",
+	      url: link,
+	      data: input
+	    })
 	})
 
 });
