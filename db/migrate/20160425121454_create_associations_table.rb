29e25d0bd8e7efdf7c3c496ca5a05beb3c515ce9
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-04-25 13:23:33 -0700

    Fix name on association table

--- db/migrate/20160425121454_create_associations_table.rb
+++ db/migrate/20160425121454_create_associations_table.rb
@@ -1,6 +1,6 @@
 class CreateAssociationsTable < ActiveRecord::Migration
   def change
-  	create_table :tables do |t|
+  	create_table :associations do |t|
       t.integer :origin_id, index: true
       t.string :association
       t.integer :target_id, index: true
