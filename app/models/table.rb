06996b0834c3b508e967ed668cd526ced0daf7da
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-04-26 10:59:37 -0700

    Update relationship migration with channel

--- app/models/table.rb
+++ app/models/table.rb
@@ -5,6 +5,7 @@ class Table < ActiveRecord::Base
 
   has_many :origins, :class_name => 'Relationship', :foreign_key => 'origin_id'
   has_many :targets, :class_name => 'Relationship', :foreign_key => 'target_id'
+  has_many :channels, :class_name => 'Relationship', :foreign_key => 'channel_id'
   
   validates :name, presence: true
   validates :user_id, presence: true
